adf45f03f65e25c50b4776999563fc2fed5164e9
jose.iglesias <jose.iglesias@106-14.info2>
2024-09-20 13:44:42 +0200

    Ejercicio terminado

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,16 @@
+package exercise1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.println(" *   *   *   *   *             *   *   *   *   *");
+		System.out.println("                                           ");
+		System.out.println("         *                             *     ");
+		System.out.println("                                            ");
+		System.out.println("         *                             *     ");
+		System.out.println("                                 ");
+		System.out.println(" *   *   *                     *   *   *   *   *");
+
+	}
+
+}
